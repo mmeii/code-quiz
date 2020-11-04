83f4e3b499db8a8e3f6336731f5f2c9fdb336bfb
Mengmei Tu <redmmeii@gmail.com>
2020-11-04 08:57:34 -0500

    this commit adds psedocoding and links src in html

---
+++ Assets/script.js
@@ -0,0 +1,36 @@
+/** 
+ * Criteria 
+ */
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+/**
+ * Psedocoding
+ */
+
+// define a set of questions
+// define variables
+// WHEN I click the start button
+// THEN a timer starts
+// presented with a question
+// after question is answered, show if correct or wrong
+// if wrong, time is subtracted from the clock
+// repeat with the rest of questions
+// when all questions are answered or timer reaches 0, game over
+// when game over, save initials and score
+
+
+// interval = setInterval(function()) {
+//     counter++;
+//     document.querySelector("#timer").textContent = counter;
+// }, 1000)
