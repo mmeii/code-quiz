c58cce21c7d7dfec84440a3cd6def8657df88681
Mengmei Tu <redmmeii@gmail.com>
2020-11-04 10:16:21 -0500

    this commit adds bootstrap, questions, contents in html

--- Assets/script.js
+++ Assets/script.js
@@ -19,7 +19,70 @@
  */
 
 // define a set of questions
-// define variables
+const quizQuestions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
+        answer: "d. <script>"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "c. quotes"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+        answer: "b. other arrays"
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        answer: "c. alerts"
+    },
+    {
+        question: "How do you create a function in JavaScript",
+        choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()"],
+        answer: "b. function myFunction()"
+    },
+    {
+        question: "How do you call a function named myFunction?",
+        choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()"],
+        answer: "c. myFunctions()"
+    },
+    {
+        question: "To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
+        answer: "b. =="
+    },
+    {
+        question: "The first index of an array is ____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        answer: "a. 0"
+    },
+    {
+        question: "Who invented JavaScript?",
+        choices: ["a. Douglas Crockford", "b. Sheryl Sandberg", "c. Brendan Eich"],
+        answer: "c. Brendan Eich"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        choices: ["a. if i == 5 then", "b. if i = 5 then", "c. if(i == 5)", "d. if i = 5"],
+        answer: "c. if(i == 5)"
+    },
+    {
+        question: "How do you add a comment in a JavaScript?",
+        choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment"],
+        answer: "a. //This is a comment"
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
+        answer: "a. onclick"
+    }
+];
+
+// grab references, define variables
 // WHEN I click the start button
 // THEN a timer starts
 // presented with a question
@@ -29,8 +92,11 @@
 // when all questions are answered or timer reaches 0, game over
 // when game over, save initials and score
 
+//functions
 
 // interval = setInterval(function()) {
 //     counter++;
 //     document.querySelector("#timer").textContent = counter;
 // }, 1000)
+
+//add event listeners
